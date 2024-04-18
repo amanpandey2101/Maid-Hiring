006b71819bce012515dde21347bf1c1ca60b0040
amanpandey2101 <paman7681@gmail.com>
2024-04-18 10:35:46 +0530

    status

--- client/src/UserDashboard/Application/Status.js
+++ client/src/UserDashboard/Application/Status.js
@@ -8,21 +8,21 @@ const Status = () => {
     <div className='flex md:flex-row flex-col-reverse justify-center items-center space-x-24 relative top-44 shadow-lg'>
       {status === 'Pending' ? (
         <>
-          <p className='text-xl text-orange-500 mt-8 pl-2 '>
+          <p className='text-xl text-orange-500 mt-8 pl-5 '>
             Your Application is Under Review...<br /> Please wait until hirer approves your Application.<br />Thanks for your patience!
           </p>
           <img src={require("../../assets/pendingStatus.gif")} alt='logo' className='md:h-96 h-72 mx-auto'/>
         </>
       ) : status === 'Approved' ? (
         <>
-          <p className='text-xl text-green-500 mt-8 pl-2  '>
+          <p className='text-xl text-green-500 mt-8 pl-5  '>
             Congratulations! Your Application has been Approved.<br/> Hirer will soon contact you!
           </p>
           <img src={require("../../assets/Verified.gif")} alt='logo' className='md:h-96 h-72 mx-auto'/>
         </>
       ) : status === 'Rejected' ? (
         <>
-          <p className='text-xl text-red-500 mt-8 pl-4 '>
+          <p className='text-xl text-red-500 mt-8 pl-5 '>
             We regret to inform you that your Application has been Rejected.<br/> Check your mail for further information.
           </p>
           <img src={require("../../assets/Rejected.gif")} alt='logo' className='md:h-80 h-64 mx-auto'/>
